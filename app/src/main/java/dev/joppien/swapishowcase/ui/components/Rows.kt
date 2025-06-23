cef6829e1341ce62af60cb5cd137d8585302a1c7
JantjeJoppien <jantje.joppien@gmail.com>
2025-06-23 13:32:52 +0200

    improve transport list ui

--- app/src/main/java/dev/joppien/swapishowcase/ui/components/Rows.kt
+++ app/src/main/java/dev/joppien/swapishowcase/ui/components/Rows.kt
@@ -4,12 +4,34 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import dev.joppien.swapishowcase.ui.theme.spacing
+
+@Composable
+fun IconRow(iconId: Int, text: String) =
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            painter = painterResource(id = iconId),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.primary,
+        )
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.End,
+        )
+    }
 
 @Composable
 fun EntryRow(labelString: String, entryString: String) =
--- app/src/main/java/dev/joppien/swapishowcase/ui/theme/Spacing.kt
+++ app/src/main/java/dev/joppien/swapishowcase/ui/theme/Spacing.kt
@@ -14,13 +14,19 @@ data class AppSpacing(
     val paddingLabelSides: Dp = 16.dp,
     val paddingScreenTopBottom: Dp = 16.dp,
     val paddingScreenSides: Dp = 24.dp,
+    val paddingCardTopBottom: Dp = 12.dp,
+    val paddingCardSides: Dp = 8.dp,
 
     val spacingButtonsColumn: Dp = 24.dp,
     val spacingButtonsRow: Dp = 12.dp,
     val spacingEntryColumn: Dp = 16.dp,
     val spacingEntryList: Dp = 8.dp,
+    val spacingCardGrid: Dp = 8.dp,
+    val spacingCard: Dp = 4.dp,
 
     val labelCornerRadius: Dp = 18.dp,
+
+    val gridMinSize: Dp = 160.dp,
 )
 
 val LocalSpacing = staticCompositionLocalOf { AppSpacing() }
